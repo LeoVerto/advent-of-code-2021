0e0fd026a9127e36bb3544b708b18e14af19ddbc
LeoVerto <LeoVerto@users.noreply.github.com>
2021-12-07 10:14:05 +0100

    Complete day 7

---
+++ days/07/07a.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+from aoc import get_inputs
+
+lines = get_inputs(7, False)
+
+crabs = np.array(list(map(int, lines[0].split(","))), dtype=int)
+
+avg = np.median(crabs)
+fuel = np.absolute(crabs - avg)
+
+print(sum(fuel))
