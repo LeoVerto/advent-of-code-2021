a88d15857685bbb76c4e3fed31385acf437b3a26
LeoVerto <LeoVerto@users.noreply.github.com>
2021-12-08 14:20:12 +0100

    Reduce code duplication

--- days/08/08b.py
+++ days/08/08b.py
@@ -10,13 +10,27 @@ def get_diff(a, b) -> [str]:
     return list(set(a).difference(b))
 
 
+def find_digit(inputs: [str], digits: [set[str]], length: int, digit: int, other_digit: int, delta_in_digit: int = None) -> str:
+    for i in inputs:
+        if len(i) == length:
+            if delta := get_diff(digits[other_digit], i):
+                if len(delta) == 1:
+                    if delta_in_digit is None or delta[0] in digits[delta_in_digit]:
+                        digits[digit] = set(i)
+                        inputs.remove(i)
+                        return delta[0]
+
+
 for line in lines:
     inputs = line.split(" ")[:10]
     outputs = line.split(" ")[11:]
 
+    # maps segment to rewired segment
     segmap: dict[str, str] = {}
+    # list of segment sets producing a certain digit
     digits = [set()] * 10
 
+    # 1, 4, 7, 8
     for i in inputs:
         if len(i) == 2:
             digits[1] = set(i)
@@ -27,44 +41,27 @@ for line in lines:
         elif len(i) == 7:
             digits[8] = set(i)
 
+    # a
     segmap["a"] = get_diff(digits[7], digits[1])[0]
 
     # 6, c
-    for i in inputs:
-        if len(i) == 6:
-            if (c := get_diff(digits[8], i)[0]) in digits[1]:
-                digits[6] = set(i)
-                segmap["c"] = c
-                inputs.remove(i)
-                break
+    segmap["c"] = find_digit(inputs, digits, 6, 6, 8, 1)
 
     # f
-    segmap["f"] = digits[1].difference([segmap["c"]]).pop()
+    segmap["f"] = get_diff(digits[1], segmap["c"])[0]
 
     # 5, e
-    for i in inputs:
-        if len(i) == 5:
-            if delta := get_diff(digits[6], i):
-                if len(delta) == 1:
-                    digits[5] = set(i)
-                    segmap["e"] = delta[0]
-                    inputs.remove(i)
-                    break
+    segmap["e"] = find_digit(inputs, digits, 5, 5, 6)
 
     # 9
     digits[9] = digits[8].copy()
     digits[9].remove(segmap["e"])
 
+    # filter already found digits from inputs
     inputs = list(filter(lambda x: set(x) not in digits, inputs))
 
     # 6, d
-    for i in inputs:
-        if len(i) == 6:
-            d = get_diff(digits[8], i)[0]
-            digits[0] = set(i)
-            segmap["d"] = d
-            inputs.remove(i)
-            break
+    segmap["d"] = find_digit(inputs, digits, 6, 0, 8)
 
     # 3
     for i in inputs:
@@ -76,6 +73,7 @@ for line in lines:
     # 2
     digits[2] = set(inputs[0])
 
+    # calculate output
     i = 3
     for o in outputs:
         for j in range(len(digits)):
